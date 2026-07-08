37dc0aa1f29bec7e4f77a78ac70c090040fb12e9
seungju1224ok-source <seungju1224ok@gmail.com>
2026-07-08 23:02:09 +0900

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,9 @@
+import { db } from "./firebase.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const docRef = doc(db,"visitors","stats");
 
