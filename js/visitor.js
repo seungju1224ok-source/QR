d84410516cdcc8b81c8242222c5552a74a52fbe2
seungju1224ok-source <seungju1224ok@gmail.com>
2026-07-08 23:12:58 +0900

    Update visitor.js

--- js/visitor.js
+++ js/visitor.js
@@ -2,38 +2,35 @@ import { db } from "./firebase.js";
 
 import {
   doc,
+  getDoc,
   updateDoc,
-  increment,
-  getDoc
+  increment
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const docRef = doc(db, "visitors", "stats");
+const statRef = doc(db, "visitors", "stats");
 
-async function countVisitor() {
+async function increaseVisitor() {
 
-    // 같은 브라우저에서 새로고침할 때 중복 증가 방지
-    if(sessionStorage.getItem("visited")){
+    if(sessionStorage.getItem("visited_ilwol14")){
         return;
     }
 
-    sessionStorage.setItem("visited","true");
+    sessionStorage.setItem("visited_ilwol14",true);
 
-    const snap = await getDoc(docRef);
+    const snapshot = await getDoc(statRef);
 
-    if(!snap.exists()){
-        console.log("stats document not found.");
-        return;
+    if(snapshot.exists()){
+
+        await updateDoc(statRef,{
+            total: increment(1),
+            today: increment(1),
+            week: increment(1),
+            month: increment(1),
+            year: increment(1)
+        });
+
     }
 
-    await updateDoc(docRef,{
-        total: increment(1),
-        today: increment(1),
-        week: increment(1),
-        month: increment(1),
-        year: increment(1)
-    });
-
-    console.log("Visitor counted.");
 }
 
-countVisitor();
+increaseVisitor();
