101f6e13d00df344eaed0b66d9ced4f3ed52a00e
seungju1224ok-source <seungju1224ok@gmail.com>
2026-07-08 23:17:53 +0900

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,21 +1,26 @@
 import { db } from "./firebase.js";
 
 import {
-doc,
-onSnapshot
-}
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+  doc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const ref=doc(db,"visitors","stats");
+const ref = doc(db, "visitors", "stats");
 
-onSnapshot(ref,(docSnap)=>{
+onSnapshot(ref, (docSnap) => {
 
-const d=docSnap.data();
+    if (!docSnap.exists()) return;
 
-document.getElementById("stat-total").textContent=d.total;
-document.getElementById("stat-day").textContent=d.today;
-document.getElementById("stat-week").textContent=d.week;
-document.getElementById("stat-month").textContent=d.month;
-document.getElementById("stat-year").textContent=d.year;
+    const d = docSnap.data();
+
+    document.getElementById("stat-total").textContent = d.total;
+    document.getElementById("stat-day").textContent = d.today;
+    document.getElementById("stat-week").textContent = d.week;
+    document.getElementById("stat-month").textContent = d.month;
+    document.getElementById("stat-year").textContent = d.year;
+
+    // 추가
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("stats").style.display = "grid";
 
 });
